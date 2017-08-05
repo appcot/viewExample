5018d16d53c227c261cef5b027e82f51194760af
appcot <appcot.2013@gmail.com>
2017-08-05 13:48:32 +0900

    Initial Commit

---
+++ viewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viewExample
+//
+//  Created by appcot on 2017/08/05.
+//  Copyright © 2017年 appcot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
